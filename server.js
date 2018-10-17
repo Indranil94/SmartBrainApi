357ccd6c3525f3699b646f8cf1aa48903d2241b9
Indranil94 <indra.santra@gmail.com>
2018-10-18 01:19:09 +0530

    Fixing bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bcrypt = require('./npm/npm-bcrpypt/bcrypt');
+const bcrypt = require('./npm/npm-bcrpypt/node-modules/bcrypt/bcrypt.js');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const knex = require('knex');
