675d5748c54b877a29b79c074d73e9f626e1f111
Indranil94 <indra.santra@gmail.com>
2018-10-18 15:43:21 +0530

    API KEY hide

--- controllers/api.js
+++ controllers/api.js
@@ -6,7 +6,7 @@ const clar_app = new Clarifai.App({
 const handleApi = (req,res) => {
 	const { imageUrl } = req.body;
 	console.log(clar_app);
-	clar_app.models.predict("a403429f2ddf4b49b307e318f00e528b",imageUrl)
+	clar_app.models.predict(process.env.KEY_CLARIFAI,imageUrl)
       .then(response=> {
 			const data = response.outputs[0].data.regions[0].region_info.bounding_box;
 			res.json(data);
